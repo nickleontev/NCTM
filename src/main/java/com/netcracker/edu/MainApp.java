8e18a3876d92b0ac72de807b4f6bf18dd6c42a95
nickleontev <nickleontevqa@gmail.com>
2019-03-18 21:12:57 +0400

    +Уведомления +Изменения незначительные изменения на форме +JAXB аннотирование с соответсвующими xmladapters

--- src/main/java/com/netcracker/edu/MainApp.java
+++ src/main/java/com/netcracker/edu/MainApp.java
@@ -1,44 +1,27 @@
 package com.netcracker.edu;
 
-import com.netcracker.edu.fxmodel.Project;
 import com.netcracker.edu.fxmodel.Root;
-import com.netcracker.edu.util.JsonUtil;
 import com.netcracker.edu.util.RuntimeDataHolder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-
 public class MainApp extends Application {
 
-    public static final String PATH = "data.json";
+    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
     private Root data = RuntimeDataHolder.getHolder();
-    private static final Logger log = LoggerFacto
+
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
-    private static Root load (){
-        Root root = JsonUtil.load(PATH);
-        if (root==null) {
-            root = new Root();
-            JsonUtil.save(root,PATH);
-        }
-        return root;
-    }
-
     public void start(Stage stage) throws Exception {
 
-        //RuntimeDataHolder.setHolder(load());
         log.info("Starting Hello JavaFX and Maven demonstration application");
 
         String fxmlFile = "/fxml/MainWindow.fxml";
@@ -55,43 +38,11 @@ public class MainApp extends Application {
         Scene scene = new Scene(rootNode, 400, 200);
         // scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("NCTM");
+        stage.setTitle("Hello JavaFX and Maven");
         stage.setScene(scene);
         stage.setMinHeight(500);
         stage.setMinWidth(500);
         stage.setMaximized(true);
-
-
-
         stage.show();
-
-        Thread manager = new Thread(new NotificationManager(),"MyThread");
-        manager.start();
-    }
-
-    public Project loadPersonDataFromFile(String path) {
-        File file = new File(path);
-        Project wrapper = null;
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(Project.class);
-            Unmarshaller um = context.createUnmarshaller();
-
-            // Чтение XML из файла и демаршализация.
-            wrapper = (Project) um.unmarshal(file);
-            data.setRoot(wrapper);
-
-
-
-        } catch (Exception e) { // catches ANY exception
-            e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Could not load data");
-            alert.setContentText("Could not load data from file:\n" + file.getPath());
-
-            alert.showAndWait();
-        }
-        return wrapper;
     }
 }
